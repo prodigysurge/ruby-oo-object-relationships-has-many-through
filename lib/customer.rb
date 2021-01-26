708a1cb74ab5960604cc99a3cd0316ff2105ccfa
prodigysurge <boatinglife6@yahoo.com>
2021-01-26 16:20:44 -0500

    commit tree d0221e9c1be8437e790a12365e86a5cdb4a92b7e

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,61 @@
 class Customer
-  
+
+    @@all = []
+    
+    attr_accessor :name, :age
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+       @@all
+    end
+
+    def name
+        @name
+    end
+
+    def new_meal(waiter, total, tip = 0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
+
+    def new_meal_20_percent(waiter, total)
+        tip = total * 0.2
+        Meal.new(waiter, self, total, tip)
+      end
+    
+      def self.oldest_customer
+        oldest_age = 0
+        oldest_customer = nil
+        self.all.each do |customer|
+          if customer.age > oldest_age
+            oldest_age = customer.age
+            oldest_customer = customer
+        end
+    end
+    oldest_customer
+    end
+
+    def customer
+        @customer
+    end
+
+
+
 end
+
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,36 @@
 class Meal
 
+    @@all = []
+
+    attr_accessor :waiter, :customer, :total, :tip
+
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer
+        @customer
+    end
+
+    def total
+        @total
+    end
+
+    def tip
+        @tip
+    end
+
+    def waiter
+        @waiter
+    end
+
+
 end
